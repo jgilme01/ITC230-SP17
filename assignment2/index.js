3ad7c0f3a2ad9e84466320d42bcfb0e60df317e5
JGilme <jgilme01@seattlecentral.edu>
2017-04-18 07:32:43 +0000

    Finished assignment

--- assignment2/index.js
+++ assignment2/index.js
@@ -1,6 +1,6 @@
-var http = require("http"), fs = require("fs"), qs = require("querystring"); var pizza = require("./pizza.js");
+'use strict'
 
-//import get from 'pizza';//imports default query function from pizza object
+var http = require("http"), fs = require("fs"), qs = require("querystring"); var pizza = require("./pizza.js");
 
 function serveFile(res, path, contentType, responseCode) {
     if(!responseCode) responseCode = 200;
@@ -12,47 +12,45 @@ function serveFile(res, path, contentType, responseCode) {
             res.writeHead(responseCode, { 'Content-Type': contentType });
             res.end(data);
         }
-        
     });
 }
 
 http.createServer(function(req,res) {
-    
-    //console.log("url =" + req.url)
-    //console.log("dir =" + __dirname)
     var url = req.url.split("?");//to split query on ? for search
     var query = qs.parse(url[1]);//to separate remainder of into objects
-    var path = url[0].toLowerCase();
-    console.log(query);
+    var path = url[0].toLowerCase();//casing for consistency
     
     switch(path) {
         case '/':
             serveFile(res, '/public/home.html', 'text/html');
-        
-       /* fs.readFile(__dirname + '/public/home.html', function read(err, data){
-            if (err) {throw err;}
-            res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end(data);})
-            res.writeHead(200, {'Content_Type':'text/plain'});
-            res.end(__dirname + '/public/home.html')*/
-        
-    
-        //res.end('Home page')
         break;
+        
         case '/about':
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('About page')
         break;
+        
         case '/get':
-        //import get from 'pizza';//imports default query function from pizza object
-        var results = pizza.get(query.name); //pass name object from query to pizza object as name
+        //imports get function from pizza object
+        var got = pizza.get(query.name); //pass name object from query to pizza object as name
         res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('For a search, enter "?" + search category + "=" + query. "?name=neopolitan"')
-        res.end(JSON.stringify(results));
+        res.write('For a search, enter "?" + search category + "=" + query. Example: "?name=neopolitan"\n')
+        let resultsGet = (got) ? JSON.stringify(got) : "Nothing returned"; //convert to string object, if returned, if not, signal nothing returned
+        res.end('Results of ' + query.name + ': \n' + resultsGet);
         break;
+        
+        case '/delete':
+        var deleted = pizza.delete(query.name);//call to pizza object, delete function with same query path
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write('To delete, enter "?" + delete category + "=" + query. Example: "?name=neopolitan"\n')
+        //let resultsDel = (deleted) ? JSON.stringify(deleted) : "Doesn't exist, can't delete";
+        res.end('Deleted: ' + query.name + '\n' + JSON.stringify(deleted))
+        break;
+        
         default:
         res.writeHead(404, {'Content-Type': 'text/plain'});
         console.log(404);
         res.end('Not Found');
     }
+    
 }).listen(process.env.PORT || 3000);
